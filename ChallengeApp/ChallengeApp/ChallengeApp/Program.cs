e1c092be475376df7707df5dcd5f561df18deb2c
Niko Post <nikodem.postrozny@gmail.com>
2023-02-04 00:52:04 +0100

    Zadanie dzien 4ty

--- ChallengeApp/ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa ";
+bool female = true;
+var age = 33;
+
+if (female && name != "Ewa " && age > 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Ewa " && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (female == false && age >= 18)
+{
+    Console.WriteLine("niepelnoletni mezczyzna");
+}
