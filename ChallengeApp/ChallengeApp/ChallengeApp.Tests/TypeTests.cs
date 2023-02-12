459c258f0685c82bafd6ae00d3fe59ed255afbc9
Niko Post <nikodem.postrozny@gmail.com>
2023-02-12 15:55:06 +0100

    Zadanie dzien 8my

---
+++ ChallengeApp/ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,62 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void WhenTwoDiffrentEmployeesNamesShouldShowEqual()
+        {
+            // arrange
+            var employee0 = GetEmployee("Angelika");
+            var employee2 = GetEmployee("Andrzej");
+
+            // act
+
+            // assert
+            Assert.AreNotEqual(employee0, employee2);
+        }
+
+        [Test]
+        public void WhenScoresAreTheSameShouldReturnEqual()
+        {
+            // arrange
+            int Score1 = 5;
+            int Score2 = 5;
+
+            // act
+
+            // assert
+            Assert.AreEqual(Score1, Score2);
+        }
+
+        [Test]
+        public void WhenFloatsAreDiffrentShouldShowEqual()
+        {
+            // arrange
+            float number1 = 2.115f;
+            float number = 5.187f;
+
+            // act
+
+            // assert
+            Assert.AreNotEqual(number1, number);
+        }
+
+        [Test]
+        public void WhenStringReturnDifferentJobsShouldShowEqual()
+        {
+            // arrange
+            string employee0 = "Journalist";
+            string employee3 = "Programmer";
+
+            // act
+
+            // assert
+            Assert.AreNotEqual(employee0, employee3);
+        }
+        private Employee GetEmployee(string Login)
+        {
+            return new Employee(Login);
+        }
+    }
+}
+
